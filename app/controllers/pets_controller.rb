713ac53f72fd5dfc61fcf0c720ccade36eac1fac
danko beribak <beribakdanko@gmail.com>
2019-08-12 17:01:38 +0200

    pets controller index and new/create and pets new view with simple form

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,10 +1,21 @@
 class PetsController < ApplicationController
   def index
+    @pets = Pet.all
   end
 
   def new
+    @pet = Pet.new
   end
 
   def create
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+    @pet.save
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :species, :price_per_day, :age, :category)
   end
 end
