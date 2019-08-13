bd70c593051cecf869ef603b51c6b12839dab1da
Arman Balani <arubalani@gmail.com>
2019-08-13 17:49:46 +0200

    added some more validations and relationships

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,9 +1,9 @@
 class Pet < ApplicationRecord
-
   CATEGORIES = ["Domestic Animals", "Exotic Animals", "Mythical Creatures", "Aquatic Animals", "Insects"]
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :users, through: :booking
+  has_many :reviews, through: :booking
   validates :name, presence: true
   validates :price_per_day, presence: true
   validates :species, presence: true
