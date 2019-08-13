f7335f05b89ab51dc94507e772236f309fea7ba1
Khoa Le <khoatle165@gmail.com>
2019-08-13 11:18:56 +0200

    create model, controller for booking

---
+++ db/migrate/20190813091750_create_bookings.rb
@@ -0,0 +1,13 @@
+class CreateBookings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bookings do |t|
+      t.date :start_date
+      t.date :end_date
+      t.integer :total_price
+      t.references :user, foreign_key: true
+      t.references :pet, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
