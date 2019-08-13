ab1fcc1d2e8e22faeab893f7c5e8328efd57b983
Khoa Le <khoatle165@gmail.com>
2019-08-13 13:01:16 +0200

    working on edit and index page for booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,35 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @bookings = Booking.where(user: current_user)
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.pet = Pet.find(params[:pet_id])
+    @booking.save
+    redirect_to bookings_path
+  end
+
+  def edit
+
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :total_price)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
 end
