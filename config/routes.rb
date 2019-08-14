fec652a3d157343aaececf6dbeaa32e7a2f8ae0b
Khoa Le <khoatle165@gmail.com>
2019-08-14 14:01:08 +0200

    search in controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :pets do
+    member do                             # member => restaurant id in URL
+      get '/search', to 'pets#search'                         # RestaurantsController#chef
+    end
+
     resources :bookings, only: [:create]
   end
   resources :bookings, only: [:index, :show, :edit, :update, :destroy] do
