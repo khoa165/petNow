0ed509bf7b8001c32853777d2252d9c8bb0fd26c
danko beribak <beribakdanko@gmail.com>
2019-08-15 18:01:22 +0200

    map layout in pets search

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -17,7 +17,8 @@ class PetsController < ApplicationController
     @markers = @pets.map do |pet|
       {
         lat: pet.latitude,
-        lng: pet.longitude
+        lng: pet.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { pet: pet })
       }
     end
   end
