6f7c52af83cbdee1508b7513f0b43a0091891f4c
Khoa Le <khoatle165@gmail.com>
2019-08-15 07:36:45 +0200

    fix some front end on show pet page

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,13 +1,14 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css"
-import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
+// import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
 flatpickr(".datepicker", {
-  allowInput: true,
-  enableTime: true
+  // allowInput: true,
+  // enableTime: true
 })
-flatpickr("#range_start", {
-  // disableMobile: true,
-  altInput: true,
-  plugins: [new rangePlugin({ input: "#range_end"})]
-})
+
+// flatpickr("#range_start", {
+//   disableMobile: true,
+//   altInput: true,
+//   plugins: [new rangePlugin({ input: "#range_end"})]
+// })
