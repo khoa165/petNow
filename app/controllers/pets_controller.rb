4cd06f8135992a7f0f267dc8120f979ed783209f
danko beribak <beribakdanko@gmail.com>
2019-08-16 12:06:38 +0200

    seed populate

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,5 +1,5 @@
 class PetsController < ApplicationController
-  skip_before_action :authenticate_user, only: [:search]
+  skip_before_action :authenticate_user!, only: [:search]
   before_action :set_pet, only: [:show, :edit, :update, :destroy]
 
   def index
